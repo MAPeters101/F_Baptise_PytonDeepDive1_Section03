8bed29a55929e194023ff06c939cd5c345acb96d
Mark Peters <mapeters101@earthlink.net>
2024-11-04 15:11:59 -0500

    Lesson020:  Complete work on lesson from 13:10.

--- lesson021.py
+++ lesson021.py
@@ -22,3 +22,22 @@ my_dict['key3'] = 10.5
 print(my_dict)
 print(hex(id(my_dict)))
 
+
+t = (1, 2, 3)
+print(t)
+print(hex(id(t)))
+print(hex(id(t[0])))
+print(hex(id(t[1])))
+
+t = ([1,2], [3,4])
+print(t)
+print(hex(id(t)))
+print(hex(id(t[0])))
+print(hex(id(t[1])))
+
+t[0].append(3)
+print(t)
+print(hex(id(t)))
+print(hex(id(t[0])))
+print(hex(id(t[1])))
+
