7977778a05c5f7fe32f996a8f174bdd54a69ccc1
Mark Peters <mapeters101@earthlink.net>
2024-11-04 12:03:21 -0500

    Lesson017:  Complete work on lesson from 9:00.

--- lesson017.py
+++ lesson017.py
@@ -17,6 +17,36 @@ def ref_count(address: int):
 
 print(ref_count(id(a)))
 
+print('b'*10)
+b = a
+print(id(b))
+print(sys.getrefcount(b))
+print(ref_count(id(b)))
+
+print('c'*10)
+c = a
+print(id(c))
+print(sys.getrefcount(c))
+print(ref_count(id(c)))
+
+print('-'*10)
+c = 10
+print(id(a))
+print(sys.getrefcount(a))
+print(ref_count(id(a)))
+
+print('='*10)
+b = None
+print(id(b))
+print(sys.getrefcount(a))
+print(ref_count(id(a)))
+
+print('+'*10)
+a_id = id(a)
+a = None
+print(id(a))
+print(sys.getrefcount(a_id))
+print(ref_count(id(a_id)))
 
 
 
