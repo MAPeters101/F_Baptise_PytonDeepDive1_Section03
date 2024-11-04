96be21e80f450db90bd31ce19111224ba2816938
Mark Peters <mapeters101@earthlink.net>
2024-11-04 13:32:52 -0500

    Lesson020:  Work on lesson.

---
+++ lesson020.py
@@ -0,0 +1,18 @@
+my_var = 10
+print(hex(id(my_var)))
+
+my_var = 15
+print(hex(id(my_var)))
+
+my_var = my_var + 10
+print(hex(id(my_var)))
+
+
+a = 10
+b = 10
+print(hex(id(a)))
+print(hex(id(b)))
+
+
+
+
