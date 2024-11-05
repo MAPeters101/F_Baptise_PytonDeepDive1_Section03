7da9d524cea5c0d02fbd93a64f112154b8418c14
Mark Peters <mapeters101@earthlink.net>
2024-11-05 12:33:35 -0500

    Lesson028:  Complete work on lesson from 10:40.

--- lesson028.py
+++ lesson028.py
@@ -1,3 +1,6 @@
+from enum import member
+
+
 def my_func():
     a = 24 * 60
     b = (1, 2) * 5
@@ -7,6 +10,51 @@ def my_func():
     f = ['a', 'b'] * 3
 
 print(my_func.__code__.co_consts)
+
+def my_func(e):
+    if e in [1, 2, 3]:
+        pass
+
+print(my_func.__code__.co_consts)
+
+def my_func(e):
+    if e in {1, 2, 3}:
+        pass
+
+print(my_func.__code__.co_consts)
+
+
+import string
+import time
+
+print(string.ascii_letters)
+char_list = list(string.ascii_letters)
+char_tuple = tuple(string.ascii_letters)
+char_set = set(string.ascii_letters)
+
+print(char_list)
+print(char_tuple)
+print(char_set)
+
+def membership_test(n, container):
+    for i in range(n):
+        if 'z' in container:
+            pass
+
+start = time.perf_counter()
+membership_test(10_000_000, char_list)
+end = time.perf_counter()
+print('list: ', end-start)
+
+start = time.perf_counter()
+membership_test(10_000_000, char_tuple)
+end = time.perf_counter()
+print('tuple: ', end-start)
+
+start = time.perf_counter()
+membership_test(10_000_000, char_set)
+end = time.perf_counter()
+print('set: ', end-start)
 
 
 
