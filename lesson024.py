259392fdc002b11869001ee194adb4fd4905524a
Mark Peters <mapeters101@earthlink.net>
2024-11-05 10:18:12 -0500

    Lesson024:  Work on lesson up to 12:00.

---
+++ lesson024.py
@@ -0,0 +1,57 @@
+a = 10
+b = a
+
+print(hex(id(a)))
+print(hex(id(b)))
+print(a is b)
+print(a == b)
+print()
+
+a = 50000000000
+b = 50000000000
+print(hex(id(a)))
+print(hex(id(b)))
+print(a is b)
+print(a == b)
+print()
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+print(hex(id(a)))
+print(hex(id(b)))
+print(a is b)
+print(a == b)
+print('-'*10)
+
+a = 10
+b = 10.0
+print(hex(id(a)))
+print(hex(id(b)))
+print(a is b)
+print(a == b)
+print()
+
+a = 10 + 0j
+b = 10.0
+print(hex(id(a)))
+print(hex(id(b)))
+print(a is b)
+print(a == b)
+print()
+
+a = 'hello'
+b = 'hello'
+print(hex(id(a)))
+print(hex(id(b)))
+print(a is b)
+print(a == b)
+print()
+
+
+
+
+
+
+
+
+
