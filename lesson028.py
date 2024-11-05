5a5edc54fec181f90e0ee0c398d156476c0e508b
Mark Peters <mapeters101@earthlink.net>
2024-11-05 12:16:18 -0500

    Lesson028:  Work on lesson up to 10:40.

---
+++ lesson028.py
@@ -0,0 +1,14 @@
+def my_func():
+    a = 24 * 60
+    b = (1, 2) * 5
+    c = 'abc' * 3
+    d = 'ab' * 11
+    e = 'the quick brown fox' * 5
+    f = ['a', 'b'] * 3
+
+print(my_func.__code__.co_consts)
+
+
+
+
+
