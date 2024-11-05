b1259993ed0542ea75bf97c342ffa167c9144730
Mark Peters <mapeters101@earthlink.net>
2024-11-05 09:19:14 -0500

    Lesson022:  Work on lesson to 14:30.

---
+++ lesson022.py
@@ -0,0 +1,25 @@
+def process(s):
+    print('Initial s # = {0}'.format(hex(id(s))))
+    s = s + ' world'
+    print('Final s # = {0}'.format(hex(id(s))))
+
+my_var = 'hello'
+print('my_var # = {0}'.format(hex(id(my_var))))
+process(my_var)
+print('my_var # = {0}'.format(hex(id(my_var))))
+print('-'*10)
+
+
+def modify_list(lst):
+    print('Initial lst # = {0}'.format(hex(id(lst))))
+    lst.append(100)
+    print('Final lst # = {0}'.format(hex(id(lst))))
+
+my_list = [1, 2, 3]
+print('my_list # = {0}'.format(hex(id(my_list))))
+modify_list(my_list)
+print('my_list # = {0}'.format(hex(id(my_list))))
+print(my_list)
+
+
+
